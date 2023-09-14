f8c03cfeb4bdfeddd9e5159d185fd425d88afa21
Killian.Sb <TheKller29@Gmail.com>
2023-09-14 15:08:32 +0200

    Ajout page templete detail + modif url

--- sondages/urls.py
+++ sondages/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "sondages"
 urlpatterns = [
     # ex: /index/
     path("", views.index, name="index"),
     # ex: /sondages/5/
+    # la valeur 'name' appelée par la balise de modèle {% url %}
     path("<int:question_id>/", views.detail, name="detail"),
     # ex: /sondages/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
--- sondages/views.py
+++ sondages/views.py
@@ -1,22 +1,18 @@
-from django.shortcuts import render
-
 # Create your views here.
 
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from sondages.models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("sondages/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "sondages/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("Vous regardez la question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "sondages/detail.html", {"question": question})
 
 
 def results(request, question_id):

37ec304dbe92809915c50aaa061cf60ec2237515
Killian.Sb <thekller29@gmail.com>
2023-09-15 09:43:21 +0200

    Mise en place des vues génériques

--- sondages/urls.py
+++ sondages/urls.py
@@ -4,13 +4,8 @@ from . import views
 
 app_name = "sondages"
 urlpatterns = [
-    # ex: /index/
-    path("", views.index, name="index"),
-    # ex: /sondages/5/
-    # la valeur 'name' appelée par la balise de modèle {% url %}
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /sondages/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /sondages/5/vote/
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- sondages/views.py
+++ sondages/views.py
@@ -3,24 +3,26 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from sondages.models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "sondages/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "sondages/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "sondages/detail.html", {"question": question})
+    def get_queryset(self):
+        """Renvoyez les cinq dernières questions publiées."""
+        return Question.objects.order_by("-pub_date")[:5]
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "sondages/detail.html"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "sondages/results.html", {"question": question})
-
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "sondages/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)

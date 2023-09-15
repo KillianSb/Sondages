12b1fa7365f11a50a8c88c0b3a6adfe9028fb5cd
Killian.Sb <thekller29@gmail.com>
2023-09-15 10:02:15 +0200

    Mise en place de test

--- sondages/tests.py
+++ sondages/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    """
+    was_published_recently() renvoie False pour les questions dont pub_date
+    est âgé de plus d’un jour.
+    """
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    """
+    was_published_recently() renvoie True pour les questions dont pub_date
+    est dans le dernier jour.
+    """
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+    """
+    was_published_recently() renvoie False pour les questions dont pub_date
+    est dans le futur.
+    """
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
